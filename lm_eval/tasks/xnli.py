ced05d4a958f19f7f4d57df0bac95831bc8a04f0
Niyati Bafna <niyatibafna13@gmail.com>
2024-04-19 08:51:02 -0400

    Decreasing few shot

--- lm_eval/tasks/xnli.py
+++ lm_eval/tasks/xnli.py
@@ -49,7 +49,7 @@ class XNLIBase(Task):
     NEUTRAL_LABEL = None  # 'Also'
     CONTRADICTION_LABEL = None  # 'No'
 
-    NUM_FEW_SHOT = 25
+    NUM_FEW_SHOT = 10
 
     def has_training_docs(self):
         return True
