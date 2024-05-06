b19ca771fa0684c4e4b990c9115b3e7bb9f0ef85
Niyati Bafna <niyatibafna13@gmail.com>
2024-05-06 15:53:35 -0400

    Adding OpenAI functionality

--- lm_eval/models/__init__.py
+++ lm_eval/models/__init__.py
@@ -3,6 +3,7 @@ from . import gpt3
 from . import huggingface
 from . import textsynth
 from . import dummy
+from . import openai
 
 MODEL_REGISTRY = {
     "hf": gpt2.HFLM,
@@ -11,6 +12,7 @@ MODEL_REGISTRY = {
     "hf-seq2seq": huggingface.AutoSeq2SeqLM,
     "gpt2": gpt2.GPT2LM,
     "gpt3": gpt3.GPT3LM,
+    "openai": openai.OpenAILM,
     "textsynth": textsynth.TextSynthLM,
     "dummy": dummy.DummyLM,
 }
